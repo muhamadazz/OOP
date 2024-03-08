947eb9d00c635028488199079a1c299a24a7a5f3
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2024-03-08 07:42:43 +0700

    Update RightTriangle.java

--- session3/src/RightTriangle.java
+++ session3/src/RightTriangle.java
@@ -1,5 +1,11 @@
 public class RightTriangle extends Triangle {
-    public RightTriangle(){
-        
+    public RightTriangle(String name, double base, double height, double hypotenuse){
+        side1 = base;
+        side2 = height;
+        side3 = hypotenuse;
+
+        setName(name);
+        calculateAreaPyth();
+        calculatePerimeter();
     }
 }
