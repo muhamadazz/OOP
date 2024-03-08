19bc1b34e725ccefefc5daa6034449af1ad902f1
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2024-03-08 07:43:04 +0700

    Update ScaleneTriangle.java

--- session3/src/ScaleneTriangle.java
+++ session3/src/ScaleneTriangle.java
@@ -1,3 +1,11 @@
-public class ScaleneTriangle {
-    
+public class ScaleneTriangle extends Triangle{
+    public ScaleneTriangle (String name, double a, double b, double c){
+        side1 = a;
+        side2 = b;
+        side3 = c;
+
+        setName(name);
+        calculateArea();
+        calculatePerimeter();
+    }
 }
