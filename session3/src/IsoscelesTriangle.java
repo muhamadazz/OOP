7b4426c4cc11db78d9bc68920675626e0c49fada
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2024-03-08 07:42:00 +0700

    Update IsoscelesTriangle.java

--- session3/src/IsoscelesTriangle.java
+++ session3/src/IsoscelesTriangle.java
@@ -1,3 +1,12 @@
-public class IsoscelesTriangle {
-    
+public class IsoscelesTriangle extends Triangle{
+    public IsoscelesTriangle(String name, double base, double leg){
+        side1 = base;
+        side2 = leg;
+        side3 = leg;
+
+        setName(name);
+        calculateArea();
+        calculatePerimeter();
+
+    }
 }
