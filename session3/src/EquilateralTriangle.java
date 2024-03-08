4fb417ebf0c51675e82e08c850d91cd2b2721a01
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2024-03-08 07:41:38 +0700

    Update EquilateralTriangle.java

--- session3/src/EquilateralTriangle.java
+++ session3/src/EquilateralTriangle.java
@@ -1,3 +1,13 @@
-public class EquilateralTriangle {
+public class EquilateralTriangle extends Triangle{
+    public EquilateralTriangle (String name, double side){
+        side1 = side;
+        side2 = side;
+        side3 = side;
+
+        setName(name);
+        calculateArea();
+        calculatePerimeter();
+
+    }
     
 }
