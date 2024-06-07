7bc4a0834f48dde96f1e87aff683adfeb391a0a4
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2024-06-07 09:58:47 +0700

    Delete libraryApp directory

--- libraryApp/src/main/java/com/azz/libraryApp/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package com.azz.libraryApp.repository;
-
-import com.azz.libraryApp.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// BookRepository.java
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
